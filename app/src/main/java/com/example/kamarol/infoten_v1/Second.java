b7241b4b54c84d62b298e3eb61e64e93926ac353
Musyrif <mucip999@gmail.com>
2017-11-05 17:06:31 +0800

    second java

--- app/src/main/java/com/example/kamarol/infoten_v1/Second.java
+++ app/src/main/java/com/example/kamarol/infoten_v1/Second.java
@@ -3,9 +3,11 @@ package com.example.kamarol.infoten_v1;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -26,4 +28,14 @@ public class Second extends Fragment {
         return inflater.inflate(R.layout.fragment_second, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        Button button = (Button) getActivity().findViewById(R.id.button2);
+        button.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                //todo
+            }
+        });
+        super.onActivityCreated(savedInstanceState);
+    }
 }
