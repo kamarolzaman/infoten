a639bfe6a43dd0bf29ba410ca2103e72b1a3d12b
Musyrif <mucip999@gmail.com>
2017-11-21 19:53:20 +0800

    Ledger balance opens payment page

--- app/src/main/java/com/example/kamarol/infoten_v1/MenuFragments/LedgerFragment.java
+++ app/src/main/java/com/example/kamarol/infoten_v1/MenuFragments/LedgerFragment.java
@@ -2,7 +2,9 @@ package com.example.kamarol.infoten_v1.MenuFragments;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.annotation.Nullable;
@@ -21,16 +23,23 @@ import com.example.kamarol.infoten_v1.R;
  * A simple {@link Fragment} subclass.
  */
 public class LedgerFragment extends Fragment {
-    TextView welcome, name;
+    Button makePaymentBtn;
     public LedgerFragment() {
         // Required empty public constructor
     }
-    Communicator comm;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_ledger, container, false);
-
+        makePaymentBtn = view.findViewById(R.id.payment);
+        makePaymentBtn.setOnClickListener(new View.OnClickListener() { //INSTEAD YOU EMBED A LISTENER TO THAT BUTTON
+            @Override
+            public void onClick(View v) {//WHEN THE EMBEDED LISTENER GET AN onClick, IT WILL RUN THIS METHOD
+                String url = "http://"+LoginFragment.username+":"+LoginFragment.password+"@info.uniten.edu.my/info/Ticketing.ASP?WCI=LedgerBalance";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
         return view;
     }
 }
