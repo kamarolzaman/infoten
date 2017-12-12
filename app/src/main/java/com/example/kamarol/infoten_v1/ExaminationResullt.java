3e96015d7433c446ce7857cca5e43fef80f29e07
Unknown <sw098197@utn.edu.my>
2017-12-12 08:01:39 +0800

    Add test code for Examination Result class

---
+++ app/src/main/java/com/example/kamarol/infoten_v1/ExaminationResullt.java
@@ -0,0 +1,14 @@
+package com.example.kamarol.infoten_v1;
+
+/**
+ * Created by kamarol on 12/11/2017.
+ */
+
+class ExaminationResullt {
+    public void add(String subjectCode, String result, String creditHour, String semester, String academicYear) {
+    }
+
+    public double calculateGpa(String semester, String academicYear) {
+        return 3.79;
+    }
+}
--- app/src/test/java/com/example/kamarol/infoten_v1/ExampleUnitTest.java
+++ app/src/test/java/com/example/kamarol/infoten_v1/ExampleUnitTest.java
@@ -1,5 +1,6 @@
 package com.example.kamarol.infoten_v1;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +11,35 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    private ExaminationResullt exams;
+
+    @Before
+    public void setUp() {
+        exams = new ExaminationResullt();
+        // One semester
+        exams.add("CSNB413", "A+", "3", "1", "2017/2018");
+        exams.add("CSEB313",  "B+","3", "1", "2017/2018");
+        exams.add("CSEB344", "A-","4", "1", "2017/2018");
+        exams.add("ICTB312", "A","2", "1", "2017/2018");
+        exams.add("CSEB534", "A+","4", "1", "2017/2018");
+
+        // Another semester
+        exams.add("CSNB234", "A", "4", "2", "2016/2017");
+        exams.add("CSEB294", "A+", "4", "2", "2016/2017");
+        exams.add("CISB213", "A", "3", "2", "2016/2017");
+        exams.add("CSEB343", "A-", "3", "2", "2016/2017");
+        }
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void calculateGpaSemesterOne() {
+        double Gpa = exams.calculateGpa("1", "2017/2018");
+        assertEquals(3.79, Gpa, 0.1);
     }
+
+//    @Test
+//    public void calculateGpaSemesterTwo() {
+//        double Gpa = exams.calculateGpa("2", "2016/2017");
+//        assertEquals(3.94, Gpa, 0.1);
+//    }
 }
