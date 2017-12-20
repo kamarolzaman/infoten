6a40e204debac583aa08b18fe9563e77119d0b2b
Musyrif <mucip999@gmail.com>
2017-12-20 22:23:29 +0800

    Exam table guna string contoh
    sbb info bagi N/A, order tak ikut time

---
+++ app/src/main/java/com/example/kamarol/infoten_v1/MenuFragments/ExaminationTableParser.java
@@ -0,0 +1,58 @@
+package com.example.kamarol.infoten_v1.MenuFragments;
+
+import android.os.AsyncTask;
+
+import com.example.kamarol.infoten_v1.LoaderChecker;
+import com.example.kamarol.infoten_v1.Tools.NTLMSchemeFactory;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.NTCredentials;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by musyrif on 20-Dec-17.
+ */
+
+public class ExaminationTableParser extends AsyncTask <Void, Void, Void> {
+    LoaderChecker listener;
+    String username, password, html;
+    StringBuffer result = new StringBuffer();
+    public ExaminationTableParser(LoaderChecker listener, String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.listener = listener;
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+        try {
+            httpclient.getAuthSchemes().register("ntlm",
+                    new NTLMSchemeFactory());
+            httpclient.getCredentialsProvider().setCredentials(
+                    new AuthScope("info.uniten.edu.my", AuthScope.ANY_PORT),
+                    new NTCredentials(username, password, "", ""));
+
+            HttpGet request = new HttpGet("http://info.uniten.edu.my/info/Ticketing.ASP?WCI=Exam");
+            HttpResponse httpResponse = httpclient.execute(request);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    httpResponse.getEntity().getContent()));
+
+            while ((html = br.readLine()) != null) {
+                result.append(html);
+            }
+        } catch (Exception e){return null;}
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        listener.onLoad(result.toString());
+    }
+}
